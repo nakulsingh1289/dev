b7342199a7731a32a24b623d2efb563b4462c59f
Nakul Singh <nakul.singh@joshtechnologygroup.com>
2022-06-19 19:41:44 +0530

    Added third video code

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,13 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { AuthContext } from "../context/AuthContext";
+import UploadFile from "./UploadFile";
+import { database } from "../firebase";
 
 export default function Feed() {
-  const { logout } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
+  const [userData, setUserData] = useState({});
+  useEffect(() => {
+    const unSub = database.users.doc(user.uid).onSnapshot((snapshot) => {
+      setUserData(snapshot.data());
+    });
+    return () => {
+      unSub();
+    };
+  }, [user]);
+
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <h1>Feed Component</h1>
       <button onClick={logout}>Logout</button>
+      <UploadFile user={userData} />
     </div>
   );
 }
---
+++ src/Components/UploadFile.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import { database, storage } from "../firebase";
+
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import LinearProgress from "@mui/material/LinearProgress";
+import MovieIcon from "@material-ui/icons/Movie";
+import { v4 as uuidv4 } from "uuid";
+
+export default function UploadFile(props) {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (file) => {
+    if (file == null) {
+      setError("Please select a file first");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+      return;
+    }
+    if (file.size / (1024 * 1024) > 100) {
+      setError("Video cannot be larger than 100 mb");
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+      return;
+    }
+
+    const uid = uuidv4();
+    setLoading(true);
+
+    const uploadTask = storage.ref(`posts/${uid}/${file.name}`).put(file);
+
+    uploadTask.on("state_change", next_fn, error_fn, complete_fn);
+    function next_fn(snapshot) {
+      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log(`Upload is ${progress} done.`);
+    }
+    function error_fn(error) {
+      setError(error);
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+      setLoading(false);
+      return;
+    }
+
+    function complete_fn() {
+      uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+        console.log(url);
+        let obj = {
+          likes: [],
+          comments: [],
+          pId: uid,
+          pUrl: url,
+          uName: props.user.fullName,
+          uProfile: props.user.profileUrl,
+          userId: props.user.userId,
+          createdAt: database.getTimeStamp(),
+        };
+        database.posts
+          .add(obj)
+          .then(async (ref) => {
+            let res = await database.users.doc(props.user.userId).update({
+              postIds:
+                props.user.postIds != null
+                  ? [...props.user.postIds, ref.id]
+                  : [ref.id],
+            });
+            console.log(res);
+          })
+          .then(() => {
+            setLoading(false);
+          })
+          .catch((err) => {
+            setError(err);
+            setTimeout(() => {
+              setError("");
+            }, 2000);
+            setLoading(false);
+          });
+      });
+    }
+  };
+  return (
+    <div style={{ marginTop: "5rem", marginBottom: "1rem" }}>
+      {error !== "" ? (
+        <Alert severity="error">{error}</Alert>
+      ) : (
+        <>
+          <input
+            type="file"
+            accept="video/*"
+            onChange={(e) => handleChange(e.target.files[0])}
+            id="upload-input"
+            style={{ display: "none" }}
+          />
+          <label htmlFor="upload-input">
+            <Button
+              variant="outlined"
+              color="secondary"
+              component="span"
+              disabled={loading}
+            >
+              <MovieIcon />
+              &nbsp;Upload Video
+            </Button>
+          </label>
+          {loading && (
+            <LinearProgress color="secondary" style={{ marginTop: "3%" }} />
+          )}
+        </>
+      )}
+    </div>
+  );
+}
